d22c4f1640d75b83037a1dbabeadf0c28048c714
Anonymoose <anonymoose0x@proton.me>
2022-07-01 17:35:54 -0400

    whitelist to frontend

--- web.mjs
+++ web.mjs
@@ -4,10 +4,17 @@ import { registerModules } from './modules.mjs';
 class ChainSpiderWeb extends ChainSpider {
   onNode(node) {
     super.onNode(node);
+    if (node.type == 'TokenBEP20') {
+    document.getElementById('messages').innerHTML += '<p>Token is BEP20: '+node.val.name; 
+    }
   }
   
   onRelation(relation) {
     super.onRelation(relation);
+    if (relation.relation == 'is-whitelisted') {
+      document.getElementById('messages').innerHTML += '<p>'+relation.src_node.val.name+' is in the '+relation.dst_node.val.platform+' whitelist!';
+      document.getElementById('messages').innerHTML += '<img src='+relation.dst_node.val.logoURI+'>';
+    }
   }
   
   onMessage(msg) {
