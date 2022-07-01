442a0818844672a07e667147f6c554e904b78961
Anonymoose <anonymoose0x@proton.me>
2022-06-30 22:41:51 -0400

    amm tracing working

--- chainspider.mjs
+++ chainspider.mjs
@@ -3,7 +3,7 @@ class RelationSet {
     this.node = node;
     this.relations = {};
   }
-  
+   
   hasRelation(target,relation) {
     if (!this.relations.hasOwnProperty(relation)) { this.relations[relation] = []; }
     return (this.relations[relation].indexOf(target) > -1);
@@ -12,8 +12,23 @@ class RelationSet {
   addRelation(target, relation) {
     if (!this.hasRelation(target,relation)) {
       this.relations[relation].push(target);
-      console.log('<NewRelation>', this.node.toString(), relation.toString(), target.toString() );
+      //console.log('<NewRelation>', this.node.toString(), relation.toString(), target.toString() );
     }
+  }
+  
+  getRelation(relation) {
+   let r = this.relations[relation];
+   if (!r) { return null; }
+   return r[0];
+  }  
+}
+
+class Message {
+  constructor (src, node, score, msg) {
+    this.src = src;
+    this.node = node;
+    this.score = score;
+    this.msg = msg;
   }
 }
 
@@ -23,6 +38,9 @@ class Node {
     this.val = val;
     this.relations = new RelationSet(this);
     
+    this.totalScore = 0;
+    this.messages = [];
+    
     console.log('<CreatedNode>', this.toString());
   } 
   
@@ -30,6 +48,21 @@ class Node {
     let v = typeof this.val == 'string' ? this.val : JSON.stringify(this.val);
     if (v.length > 64) { v = v.substring(0,64) + '...'; }
     return `${this.type}[${v}]`;
+  }
+
+  relative(relation) {
+    return this.relations.getRelation(relation);
+  }
+  
+  reportMessage(src, score, msg) {
+    let m = new Message(src, this, score, msg);
+    this.messages.push(m);
+    
+    this.totalScore = 0;
+    for (let i=0; i < this.messages.length; i++) { this.totalScore += this.messages[i].score; }
+
+    console.log('<Message:'+src+'>', score, msg);
+    console.log('<Score>', this.toString(), this.totalScore);
   }
 }
 
@@ -54,10 +87,6 @@ class Relation {
     this.src_node = src_node;
     this.relation = relation;
     this.dst_node = dst_node;
-    
-    if (relation.substring(0,1) != '@') {
-      this.src_node.relations.addRelation(dst_node, relation);
-    }
   }
   
   toString() {
@@ -80,9 +109,17 @@ export class ChainSpider {
     this.nodes = [];
     this.relations = [];
     this.subscriptions = [];
+    this.messages = [];
   }
 
   createNode(type, val) {
+    for (let n of this.nodes) {
+      if (n.type == type && JSON.stringify(val) == JSON.stringify(n.val)) { 
+        console.log('<DupeNode>', 'not creating', type, val);
+        return;
+      }
+    }
+    
     let node = new Node(type, val);
     this.nodes.push(node);
 
@@ -97,8 +134,19 @@ export class ChainSpider {
   }
   
   createRelation(src_node, relation, dst_node) {
+    for (let r of this.relations) {
+      if (r.src_node == src_node && r.relation == relation && r.dst_node == dst_node) { 
+        console.log('<DupeRelation>', 'not creating', src_node.toString(), relation, dst_node.toString());
+        return;
+      }
+    }
+      
     let r = new Relation(src_node, relation, dst_node);
     this.relations.push(r);
+    
+    // link relation to src and dst nodes
+    src_node.relations.addRelation(dst_node, relation);
+    dst_node.relations.addRelation(src_node, relation);
     
     // fire subscriptions
     for (let s of this.subscriptions) {
@@ -107,6 +155,7 @@ export class ChainSpider {
       }
     }
 
+    console.log('<NewRelation>', r.toString());
     return r;
   }
   
--- modules.mjs
+++ modules.mjs
@@ -1,6 +1,7 @@
 import { Inspector } from './chainspider.mjs';
 import web3 from './blockchain.mjs';
-import { bep20 } from './lib/abi.mjs';
+import { bep20, pancakeLP } from './lib/abi.mjs';
+import fetch from 'node-fetch';
 
 export class ContractFinder extends Inspector {
   constructor(cs) { 
@@ -22,6 +23,45 @@ export class ContractFinder extends Inspector {
 
 }
 
+export class WhitelistChecker extends Inspector {
+  constructor(cs) { 
+    super(cs, 'WhitelistChecker');
+    this.subscribe('Contract', 'is-token');
+  }
+  
+  async onRelation(r) {
+    let addr = r.src_node.relative('is-contract').val;
+    let res = await fetch('https://github.com/pancakeswap/token-list/raw/main/src/tokens/cmc.json');
+    let tokens = await res.json();
+    
+    let { symbol, name } = r.dst_node.val;
+    let match = false;
+    for (let wt of tokens) {
+      let wtaddr = wt.address.toLowerCase();
+      
+      if (wt.name == name && wt.symbol == symbol) {
+        match = true;
+        if (wtaddr == addr) {
+          let w = this.cs.createNode( 'WhitelistedToken', { 'platform': 'cmc', 'logoURI': wt.logoURI });
+          this.cs.createRelation( r.dst_node, 'is-whitelisted', w );
+          r.dst_node.reportMessage( this.id, 100, 'Token was found in the CoinMarketCap whitelist.' );
+        } else {
+          r.dst_node.reportMessage( this.id, -100, 'Token attempting to impersonate ' + wt.name + ' but contract address is incorrect' );
+        }
+      } else if (wt.name == name) {
+        match = true;
+        r.dst_node.reportMessage( this.id, -50, 'Token attempting to impersonate ' + wt.name + ' but symbol is incorrect' );
+      }
+
+    }
+    
+    if (!match) {
+      r.dst_node.reportMessage( this.id, 0, 'Token was not found in CMC whitelist.' );
+    }
+    
+  }
+
+}
 
 export class TokenFinder extends Inspector {
   constructor(cs) { 
@@ -32,13 +72,41 @@ export class TokenFinder extends Inspector {
   async onRelation(r) {
     let addr = r.src_node.val;  
     const token_abi = await new web3.eth.Contract( bep20, addr );
-    let info;
     
+    let symbol;
+    try {
+      symbol = await token_abi.methods.symbol().call();
+    } catch(err) {
+      console.error(this.id, 'error', err);
+      return;
+    }     
+    
+    if (symbol == 'Cake-LP') {
+      let lp_abi = await new web3.eth.Contract( pancakeLP, addr );
+      let asset;
+      let base;
+      try {
+        asset = await lp_abi.methods.token0().call();
+        base = await lp_abi.methods.token1().call();
+      } catch(err) {
+        console.error(this.id, 'error', err);
+        return;
+      }
+
+      let t = this.cs.createNode('TokenAMM', { asset: asset.toLowerCase(), base: base.toLowerCase() });
+      this.cs.createRelation(r.dst_node, 'is-amm', t);
+      this.cs.createNode('BlockchainAddress', asset.toLowerCase());
+      this.cs.createNode('BlockchainAddress', base.toLowerCase());
+              
+      return;
+    }
+    
+    let info;
     try {
       info = {
         //contractAddress: addr,
         totalSupply: await token_abi.methods.totalSupply().call(),
-        symbol: await token_abi.methods.symbol().call(),
+        symbol,
         name: await token_abi.methods.name().call(),
         decimals: await token_abi.methods.decimals().call()
       }
@@ -46,13 +114,9 @@ export class TokenFinder extends Inspector {
       console.error(this.id, 'error', err);
       return;
     }
-    
-    if (info.symbol == 'Cake-LP') {
-      // TODO: get base and asset pairs
-    } else {
-      let t = this.cs.createNode('TokenBEP20', info);
-      this.cs.createRelation(r.src_node, 'is-token', t);
-    }
+
+    let t = this.cs.createNode('TokenBEP20', info);
+    this.cs.createRelation(r.dst_node, 'is-token', t);    
   }
   
 }
